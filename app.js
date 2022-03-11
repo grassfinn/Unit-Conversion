37986b29479f847de8d79e82a654a0a069526216
grassfinn <theharper@live.com>
2022-03-10 21:39:41 -0500

    user input/ style changes

--- app.js
+++ app.js
@@ -6,8 +6,15 @@ const poundsEl = document.querySelector('#pounds');
 const metersEl = document.querySelector('#meters');
 const litersEl = document.querySelector('#liters');
 const kilosEl = document.querySelector('#kilos');
+
+const input = document.getElementById('user-input')
+const submit = document.getElementById('submit')
+
 const conversion = 20;
 number.textContent = conversion;
+
+const userNumber = input.value;
+
 
 // FORMULAS
 //  f -> m =  F / 3.281
@@ -35,3 +42,36 @@ litersEl.textContent = `${conversion} liters = ${litersToGallons} `
 
 kilosEl.textContent = `${conversion} kilograms = ${kilosToPounds} `
 
+const userInputConversion = () => {
+  const conversion = input.value;
+
+  number.textContent = input.value;
+
+  // FORMULAS
+  //  f -> m =  F / 3.281
+  const feetToMeters = (conversion / 3.281).toFixed(3);
+  //  P -> K = p / 2.205
+  const poundsToKilo = (conversion / 2.205).toFixed(3);
+  // G -> L = G / 3.785
+  const gallonsToLiters = (conversion / 3.785).toFixed(3);
+  // M -> F = M * 3.281
+  const metersToFeet = (conversion * 3.281).toFixed(3);
+  // K -> P = K * 2.205
+  const kilosToPounds = (conversion * 2.205).toFixed(3);
+  // L -> G = L * 3.785
+  const litersToGallons = (conversion * 3.785).toFixed(3);
+
+  feetEl.textContent = ` ${conversion} feet = ${feetToMeters}`;
+
+  gallonsEl.textContent = ` ${conversion} gallons = ${gallonsToLiters}`;
+
+  poundsEl.textContent = ` ${conversion} pounds = ${poundsToKilo}`;
+
+  metersEl.textContent = `${conversion} meters = ${metersToFeet} `;
+
+  litersEl.textContent = `${conversion} liters = ${litersToGallons} `;
+
+  kilosEl.textContent = `${conversion} kilograms = ${kilosToPounds} `;
+}
+
+submit.addEventListener('click', () => userInputConversion())
